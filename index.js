fcf8763cf48bdfa20199d7d78408b62fafa6b745
zoemithaug <108873796+zoemithaug@users.noreply.github.com>
2022-07-25 23:51:31 -0400

    Updated code, fixed comments and spacing

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 let express = require('express');
 let nunjucks = require('nunjucks');
-// let fetch = require('node-fetch');
-// import fetch from 'fetch';
 let bodyParser = require('body-parser');
 
 // ✅ Do this if using JAVASCRIPT
@@ -31,16 +29,11 @@ SetUpDatabase();
 DisplayHomePage();
 PostSchedule();
 
-
 //Displaying Homepage 
 function DisplayHomePage() {
   //Displaying Homepage after reading an HTML file locally
   webserver.get('/', function (req, res) {
-    res.render('index.html', {
-      // name: req.params.name
-      name: 'Kevin'
-
-    })
+    res.render('index.html')
   });
 }
 
@@ -52,23 +45,19 @@ function PostSchedule() {
 
     //Student info/variables collected from HTML form 
 
-    var professorname = req.body.ProfessorName;
-    var appointmentdate = req.body.AppointmentDate;
-    var appointmenttime = req.body.AppointmentTime;
-
-    var studentfirstname = req.body.studentfirstname;
-    var studentlastname = req.body.studentlastname;
-    var studentphonenumber = req.body.studentphonenumber;
-    var studentnotes = req.body.studentnotes;
+    let professorname = req.body.ProfessorName;
+    let appointmentdate = req.body.AppointmentDate;
+    let appointmenttime = req.body.AppointmentTime;
+    let studentfirstname = req.body.studentfirstname;
+    let studentlastname = req.body.studentlastname;
+    let studentphonenumber = req.body.studentphonenumber;
+    let studentnotes = req.body.studentnotes;
 
     //Text message info sent to Student 
-    var message = "Dear " + studentfirstname + " " + studentlastname + "," + " you have successfully booked your appointment! Here are your additional notes: " + studentnotes;
-
-    //res.send(studentfirstname);
+    let message = "Dear " + studentfirstname + " " + studentlastname + "," + " you have successfully booked your appointment! Here are your additional notes: " + studentnotes;
 
     //JSON info object -> posted to Vonage -> so Vonage can send text
     let todo = {
-
       from: "18338510989",
       text: message,
       to: "18135145793",
@@ -76,9 +65,8 @@ function PostSchedule() {
       api_secret: API_Secret
     };
 
-
-    var ValidationCheck = true; //Assume success, prove otherwise
-    var ErrorMessage = '';
+    let ValidationCheck = true; //Assume success, prove otherwise
+    let ErrorMessage = '';
 
     if (appointmentdate == '') {
 
@@ -90,38 +78,30 @@ function PostSchedule() {
       ValidationCheck = false;
       ErrorMessage = ErrorMessage + '<li>Enter an appointment time</li>';
 
-
     } else if (professorname == '') {
 
       ValidationCheck = false;
       ErrorMessage = ErrorMessage + '<li>Select a Professor</li>';
-
 
     } else if (studentfirstname == '') {
 
       ValidationCheck = false;
       ErrorMessage = ErrorMessage + '<li>Enter a Student first name</li>';
 
-
     } else if (studentlastname == '') {
 
       ValidationCheck = false;
       ErrorMessage = ErrorMessage + '<li>Enter a Student last name</li>';
-
 
     } else if (studentphonenumber == '') {
 
       ValidationCheck = false;
       ErrorMessage = ErrorMessage + '<li>Enter a mobile phone number</li>';
 
-
     }
 
     if (ValidationCheck == false) {
-
-
       res.render('error.html', {
-        // name: req.params.name
         ProfessorName: professorname,
         StudentName: studentfirstname + ' ' + studentlastname,
         AppointmentDate: appointmentdate,
@@ -132,12 +112,7 @@ function PostSchedule() {
 
       })
 
-
-
-
-
     } else {
-
       //Posting data to Vonage & receiving success or failure 
       fetch('https://rest.nexmo.com/sms/json', {
         method: 'POST',
@@ -146,11 +121,8 @@ function PostSchedule() {
       }).then(res => res.json())
         .then(json => console.log(json));
 
-      // res.send(req.body);
-      // res.redirect('/');
-
       //Build SQL string --> insert string to add record to appointments table 
-      var sqlstring = 'INSERT INTO Appointments (appointmentdate, appointmenttime, professorname, studentfirstname, studentlastname, studentphonenumber, studentnotes) ' +
+      let sqlstring = 'INSERT INTO Appointments (appointmentdate, appointmenttime, professorname, studentfirstname, studentlastname, studentphonenumber, studentnotes) ' +
         'VALUES (?, ?, ?, ?, ?, ?, ?)';
 
       //Execute SQL string into database (Using paramaterized queries to prevent SQL injection)
@@ -161,64 +133,32 @@ function PostSchedule() {
         console.log(row.appointmentdate + ": " + row.appointmenttime + ":" + row.professorname);
       });
 
-
-
       res.render('confirmation.html', {
-        // name: req.params.name
         ProfessorName: professorname,
         StudentName: studentfirstname + ' ' + studentlastname,
         AppointmentDate: appointmentdate,
         AppointmentTime: appointmenttime + ':00 AM',
         StudentNotes: studentnotes,
         StudentPhoneNumber: studentphonenumber
-
       })
-
     }
-
   }); // End Webserver POST
-
-
-
 }
 
-
 function SetUpDatabase() {
-
   try {
     db.serialize(() => {
-
-
 
       db.run("CREATE TABLE if not exists Appointments (appointmentdate text, appointmenttime text, professorname text, studentfirstname text, studentlastname text, studentphonenumber text, studentnotes text)");
 
     });
 
   } catch {
-
     db.serialize(() => {
-
       //Table already created
       db.each("SELECT appointmentdate, appointmenttime, professorname, studentfirstname, studentlastname info FROM Appointments", (err, row) => {
         console.log(row.appointmentdate + ": " + row.appointmenttime + ":" + row.professorname);
       });
-
     });
-
-
   }
-
-
-  /* const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (let i = 0; i < 10; i++) {
-  stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
-  */
-
-
-
-
-  // db.close();
-
 }
